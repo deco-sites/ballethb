c32fde13a4f8b88247e0660604039e50473ac904
Lucas Leite <carvalho1104@gmail.com>
2024-09-13 00:49:53 +0000

    Publish 5 from Staging into origin/main

--- sections/Theme/Theme.tsx
+++ sections/Theme/Theme.tsx
@@ -212,7 +212,7 @@ const toVariables = (
 };
 
 const defaultTheme = {
-  "primary": "oklch(1 0 0)",
+  "primary": "#CB9C98",
   "secondary": "oklch(1 0 0)",
   "tertiary": "oklch(1 0 0)",
   "neutral": "oklch(1 0 0)",
@@ -221,7 +221,8 @@ const defaultTheme = {
   "success": "oklch(0.9054 0.1546 194.7689)",
   "warning": "oklch(1 0 0)",
   "error": "oklch(1 0 0)",
-
+  
+   "--fallback-primary": "#CB9C98",
   "--rounded-box": "1rem", // border radius rounded-box utility class, used in card and other large boxes
   "--rounded-btn": "0.2rem" as const, // border radius rounded-btn utility class, used in buttons and similar element
   "--rounded-badge": "1.9rem", // border radius rounded-badge utility class, used in badges and similar
